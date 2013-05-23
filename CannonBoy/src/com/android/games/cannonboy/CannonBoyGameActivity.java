ba4aaf162d1f4e0b381ac9420308a66cfbfe22be
Cole Sobotka <cole.sobotka@gmail.com>
2013-05-23 00:09:48 -0600

    learning as we go

--- CannonBoy/src/com/android/games/cannonboy/CannonBoyGameActivity.java
+++ CannonBoy/src/com/android/games/cannonboy/CannonBoyGameActivity.java
@@ -1,25 +1,76 @@
 package com.android.games.cannonboy;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.TiledSprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.debug.Debug;
 
-public class CannonBoyGameActivity extends Activity {
+import android.view.Display;
 
+public class CannonBoyGameActivity extends SimpleBaseGameActivity {
+
+	private static int CAMERA_WIDTH = 0;
+	private static int CAMERA_HEIGHT = 0;
+	
+	
+	private BuildableBitmapTextureAtlas mBitmapTextureAtlas;
+	private TiledTextureRegion mCannonBoyTextureRegion;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_cannon_boy_game);
+	public EngineOptions onCreateEngineOptions() {
+		
+		Display currDisplay = getWindowManager().getDefaultDisplay();
+		CAMERA_WIDTH = currDisplay.getWidth();
+		CAMERA_HEIGHT = currDisplay.getHeight();
+		
+		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+	protected void onCreateResources() {
 		
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		
-		//OMG THIS IS A TEST
-		getMenuInflater().inflate(R.menu.cannon_boy_game, menu);
-		return true;
+		this.mBitmapTextureAtlas = new BuildableBitmapTextureAtlas(this.getTextureManager(), 512, 256, TextureOptions.NEAREST);
+		
+		this.mCannonBoyTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "face_circle_tiled.png", 2, 1);
+		
+		try {
+			this.mBitmapTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0, 0, 1));
+			this.mBitmapTextureAtlas.load();
+		} catch (TextureAtlasBuilderException e) {
+			Debug.e(e);
+		}
 	}
 
+	@Override
+	protected Scene onCreateScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene();
+		scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+
+		/* Quickly twinkling face. */
+		final TiledSprite cannonBoy = new TiledSprite(100, 50, this.mCannonBoyTextureRegion, this.getVertexBufferObjectManager());
+		cannonBoy.setCurrentTileIndex(0);
+		scene.attachChild(cannonBoy);
+		return scene;
+	}
 }
---
+++ CannonBoy/src/com/android/games/cannonboy/entities/CannonBoy.java
@@ -0,0 +1,5 @@
+package com.android.games.cannonboy.entities;
+
+public class CannonBoy {
+
+}
