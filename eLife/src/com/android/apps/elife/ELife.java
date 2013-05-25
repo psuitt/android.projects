0ee9f466d2d5b57fcfffb4edcf5f701c573aa1b5
Sora <limitlessreverie@gmail.com>
2013-05-24 21:12:34 -0600

    commit

--- eLife/src/com/android/apps/elife/ELife.java
+++ eLife/src/com/android/apps/elife/ELife.java
@@ -16,7 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import com.android.apps.elife.R;
 
 public class ELife extends FragmentActivity {
 
