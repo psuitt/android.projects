aa565c4a89280716de5d851767d395b370851f55
Sora <limitlessreverie@gmail.com>
2013-05-25 17:36:59 -0600

    Fragment For Home Page
    Calendar fragment for storing calendar info.

---
+++ eLife/src/com/android/apps/elife/fragments/MainCalendarFragment.java
@@ -0,0 +1,18 @@
+package com.android.apps.elife.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Calendar fragment located left of the home page.
+ * @author Sora
+ *
+ */
+public class MainCalendarFragment extends Fragment {
+	
+	/**
+	 * Constructor does nothing.
+	 */
+	public MainCalendarFragment() {	
+	}
+
+}
