50867a38c31103780d54200b47bf5ca71bbefb9c
Sora <limitlessreverie@gmail.com>
2013-05-31 15:14:23 -0600

    removes some stuff

--- Blast/src/com/android/projects/games/blast/consts/ColorConstants.java
+++ Blast/src/com/android/projects/games/blast/consts/ColorConstants.java
@@ -10,7 +10,7 @@ import android.graphics.Color;
  */
 public final class ColorConstants {
 
-	public static final int SKY_BLUE = Color.argb(255, 135, 206, 250);
+	public static final int SKY_BLUE = Color.argb(255, 135, 196, 250);
 	
 	/** Hidden constructor. */
 	private ColorConstants() { }
