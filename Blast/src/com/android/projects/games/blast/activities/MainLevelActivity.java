0b9676bfd153b6a6a3d6ebbdf4398e53138782bc
Sora <limitlessreverie@gmail.com>
2013-05-31 12:37:53 -0600

    New stuff

--- Blast/src/com/android/projects/games/blast/activities/MainLevelActivity.java
+++ Blast/src/com/android/projects/games/blast/activities/MainLevelActivity.java
@@ -1,23 +1,30 @@
 package com.android.projects.games.blast.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.projects.games.blast.R;
 
-public class MenuActivity extends Activity {
+public class MainLevelActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_menu);
+		setContentView(R.layout.level);
+		TextView dummyTextView = (TextView) findViewById(R.id.section_label);
+		Bundle extra = getIntent().getExtras();
+		dummyTextView.setText(getIntent().getExtras().getString("level"));
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
