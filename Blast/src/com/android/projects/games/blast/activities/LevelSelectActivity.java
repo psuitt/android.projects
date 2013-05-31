6c08eb82ea4355cac3faa15ea7f008b0ec44c7e6
Sora <limitlessreverie@gmail.com>
2013-05-31 10:51:46 -0600

    Working Level select.

--- Blast/src/com/android/projects/games/blast/activities/LevelSelectActivity.java
+++ Blast/src/com/android/projects/games/blast/activities/LevelSelectActivity.java
@@ -1,5 +1,24 @@
 package com.android.projects.games.blast.activities;
 
-public class LevelSelectActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.android.projects.games.blast.R;
+
+public class LevelSelectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.level_select);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
--- Blast/src/com/android/projects/games/blast/activities/MenuActivity.java
+++ Blast/src/com/android/projects/games/blast/activities/MenuActivity.java
@@ -1,5 +1,24 @@
 package com.android.projects.games.blast.activities;
 
-public class MenuActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.android.projects.games.blast.R;
+
+public class MenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
